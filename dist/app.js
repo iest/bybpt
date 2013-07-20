599ca2e22fc5e1757013abd2fa59e50d9aa03fa2
Iestyn Williams <Iestyn.Williams@eysys.com>
2013-07-20 13:14:55 +0100

    Added in sticky nav for screens of medium size

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,36 @@
 window.onload = function() {
 
+  var doc = document.documentElement,
+      body = document.body,
+      nav = document.querySelector('.header-nav'),
+      navOffset = nav.offsetTop,
+      navHeight = nav.offsetHeight;
+
+  var floatingNav = document.querySelector('.floating-nav');
+
+  function scrollyThing() {
+    var scrolled = (doc && doc.scrollTop  || body && body.scrollTop  || 0);
+
+    if (scrolled >= navOffset) {
+      if (!floatingNav.classList.contains('nav-fixed')) {
+        floatingNav.classList.add('nav-fixed');
+        nav.classList.add('nav-hide');
+      }
+    }
+    if (scrolled < navOffset) {
+      floatingNav.classList.remove('nav-fixed');
+      nav.classList.remove('nav-hide');
+    }
+  }
+
+  window.onresize = function(){
+      navOffset = nav.offsetTop;
+  };
+
+  window.onscroll = function() {
+    scrollyThing();
+  };
+
   // Swipejs slider
   slider = new Swipe(document.querySelector('.swipe'), {
     speed: 400,
